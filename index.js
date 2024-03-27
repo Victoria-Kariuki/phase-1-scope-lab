5be0ed145571614f3dc7bd35b0776a1214df8e13
victoria <kariukivictoria428@gmail.com>
2024-03-28 01:33:03 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName='bob';
+var bestCustomer='not bob';
+const changeLeastFavoriteCustomer='Vicky';
+
+
+function upperCaseCustomerName(){
+    customerName='BOB';
+}
+
+function setBestCustomer(){
+    bestCustomer='not bob'
+    return bestCustomer;
+}
+setBestCustomer()
+
+function overwriteBestCustomer(){
+  return bestCustomer='maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer='Sam'
+}
+changeLeastFavoriteCustomer()
